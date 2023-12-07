31407f854e5c3e23dec384da733f22440cb2fc49
Irsyad Nafi <irsyadnap33@gmail.com>
2023-12-07 23:59:53 +0700

    index layout

---
+++ js/tailwind.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+  theme: {
+    screens: {
+      mini: "400px",
+    },
+    extend: {
+      colors: {
+        clifford: "#da373d",
+      },
+      backgroundImage: {
+        pantai: "url(../assets/background.gif)",
+        lucu: "url(../assets/lucu.gif)",
+      },
+    },
+  },
+};
